02095c3cd520ea0b6e70df324f8fa2a7e64ef890
Carlos Knowles <cknowles0711@gmail.com>
2020-10-28 20:02:09 -0500

    added day_month_year split and add_list functions

--- lambdata_cknowles11/__init__.py
+++ lambdata_cknowles11/__init__.py
@@ -1,5 +1,19 @@
 from .df_utils import ONES, ZEROS
+import pandas as pd
+import numpy as np
 
 __version__ = "0.0.1"
 my_str = "This is my string from __init__.py"
+day_month_year = def day_month_year(df, column):
+    df =df.copy()
+    df["day"] = df[column].dt.day
+    df["month"] = df[column].dt.month
+    df["year"] = df[column].dt.year
+    return df
+
+add_list = def add_list(df, series, name):
+    df = df.copy()
+    series = pd.Series(series)
+    df[name] = series
+    return df
 
