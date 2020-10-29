32fbb7e4052d3545256e7d9566cf79cfd902d7b8
Carlos Knowles <cknowles0711@gmail.com>
2020-10-28 20:24:23 -0500

    syntax change

--- lambdata_cknowles11/__init__.py
+++ lambdata_cknowles11/__init__.py
@@ -4,14 +4,14 @@ import numpy as np
 
 __version__ = "0.1.2"
 my_str = "This is my string from __init__.py"
-day_month_year = def day_month_year(df, column):
+def day_month_year(df, column):
     df =df.copy()
     df["day"] = df[column].dt.day
     df["month"] = df[column].dt.month
     df["year"] = df[column].dt.year
     return df
 
-add_list = def add_list(df, series, name):
+def add_list(df, series, name):
     df = df.copy()
     series = pd.Series(series)
     df[name] = series
